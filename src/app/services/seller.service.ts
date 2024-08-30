482ade237a797f4112421f23f70751abae762f00
Sherdil20 <sherdil20.am@gmail.com>
2024-08-30 15:23:27 +0500

    kchichri

--- src/app/services/seller.service.ts
+++ src/app/services/seller.service.ts
@@ -8,20 +8,19 @@ import { Router } from '@angular/router';
   providedIn: 'root'
 })
 export class SellerService {
-isSellerLoggedIn= new BehaviorSubject<boolean>(false);
+isSellerLoggedIn= new BehaviorSubject<boolean>(false)
 isLoginError= new EventEmitter<boolean>(false)
-  constructor(private http:HttpClient, private router: Router) {}
+  constructor(private http:HttpClient, private router:Router) {}
   userSignUp(data:SignUp){
      this.http.post('http://localhost:3000/seller',
       data,
       {observe:'response'}).subscribe((result)=>{
-        this.isSellerLoggedIn.next(true);
-      if(result){
+        
       localStorage.setItem('seller',JSON.stringify(result.body))
       this.router.navigate(['seller-home'])
-      }
-     })
-    }
+      })
+     }
+    
     reloadSeller(){
       if(localStorage.getItem('seller')){
         this.isSellerLoggedIn.next(true)
@@ -31,10 +30,10 @@ isLoginError= new EventEmitter<boolean>(false)
   UserLogin(data:Login){
 
   this.http
-     .get('http://localhost:3000/seller?email=${data.email}&password=${data.password}',{
+     .get(`http://localhost:3000/seller?email=${data.email}&password=${data.password}`,{
       observe:'response'}).subscribe((result:any)=>{
-        this.isSellerLoggedIn.next(true);
-        if(result && result.body &&result.body.length){
+    
+        if(result && result.body && result.body.length){
           localStorage.setItem('seller',JSON.stringify(result.body))
           this.router.navigate(['seller-home'])
         }else{
