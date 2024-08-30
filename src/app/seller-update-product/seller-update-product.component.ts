482a766c48dbd20672f11cf70ec2c231c195b53a
Sherdil20 <sherdil20.am@gmail.com>
2024-08-31 02:48:42 +0500

    Product Update with API

--- src/app/seller-update-product/seller-update-product.component.ts
+++ src/app/seller-update-product/seller-update-product.component.ts
@@ -10,15 +10,29 @@ import { product } from '../data-type';
 })
 export class SellerUpdateProductComponent implements OnInit {
 productData:undefined | product
+productMessage:undefined | string;
   constructor(private route:ActivatedRoute, private product:ProductService){}
 ngOnInit(){
   let productId=this.route.snapshot.paramMap.get('id');
     console.warn(productId);
-  productId && this.product.getproduct(productId).subscribe((data)=>{
+  productId && this.product.getProduct(productId).subscribe((data)=>{
     console.warn(data);
     this.productData=data
   })
 }
 
-  submit(data:any){}
+  submit(data:product){
+    console.warn(data);
+    if(this.productData){
+      data.id=this.productData.id;
+    }
+    this.product.updateProduct(data).subscribe((result)=>{
+      if (result){
+        this.productMessage="Product has Updated";
+      }
+    });
+    setTimeout(()=>{
+      this.productMessage= undefined;
+    }, 3000);
+  }
 }
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -9,15 +9,19 @@ export class ProductService {
 
   constructor(private http:HttpClient) { }
   addProduct(data:product){
-    return this.http.post('http://localhost:3000/products', data);
+    return this.http.post(`http://localhost:3000/products`, data);
   }
   productList(){
-    return this.http.get<product[]>('http://localhost:3000/products');
+    return this.http.get<product[]>(`http://localhost:3000/products`);
   }
   deleteProduct(id:string){
-   return this.http.delete(`http://localhost:3000/products/${id}`)
+   return this.http.delete(`http://localhost:3000/products/${id}`);
 }
-getproduct(id:string){
-  return this.http.get<product>(`http://localhost:3000/products/${id}`)
+getProduct(id:string){
+  return this.http.get<product>(`http://localhost:3000/products/${id}`);
+}
+updateProduct(product:product){
+  console.warn(product)
+  return this.http.put<product>(`http://localhost:3000/products/${product.id}`,product);
 }
 }
