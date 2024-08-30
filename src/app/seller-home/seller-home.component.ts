04ccf406af1458a8233e23d7d1233fc85f0b0fbd
Sherdil20 <sherdil20.am@gmail.com>
2024-08-31 01:06:55 +0500

    UI for Update Product

--- src/app/seller-home/seller-home.component.ts
+++ src/app/seller-home/seller-home.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../services/product.service';
 import { product } from '../data-type';
-import { faTrash } from '@fortawesome/free-solid-svg-icons';
+import { faEdit, faTrash } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-seller-home',
@@ -12,6 +12,7 @@ export class SellerHomeComponent implements OnInit {
   productList:undefined | product[];
   productMessage:undefined|string;
   icon=faTrash;
+  iconup=faEdit;
   constructor(private product:ProductService){}
 
   ngOnInit():void{
