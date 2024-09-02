beab88fc66789faa9dbe19d3024c595ce399e9ab
Sherdil20 <sherdil20.am@gmail.com>
2024-09-02 17:19:32 +0500

    Sign Up Form in angular

---
+++ src/app/user-auth/user-auth.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-auth',
+  templateUrl: './user-auth.component.html',
+  styleUrls: ['./user-auth.component.css']
+})
+export class UserAuthComponent implements OnInit {
+  constructor(){
+
+  }
+
+
+  ngOnInit(): void {
+    
+  }
+  signUp(data:FormData){
+console.warn(data);
+
+  }
+
+}
