85c8e92455e9ae55f4428c38b1f8371c7fcc86e2
Sherdil20 <sherdil20.am@gmail.com>
2024-09-17 21:28:27 +0500

    trendy products list addtocart function made functional

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../services/product.service';
-import { product } from '../data-type';
+import { product, cart } from '../data-type';
 
 @Component({
   selector: 'app-home',
@@ -8,16 +8,53 @@ import { product } from '../data-type';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  popularProducts:undefined | product[];
-  trendyProducts: undefined | product[]
-  constructor(private product:ProductService){}
-ngOnInit(): void {
-  this.product.popularProducts().subscribe((data)=>{
+  popularProducts: undefined | product[];
+  trendyProducts: undefined | product[];
 
-this.popularProducts=data;
-  });
-  this.product.trendyProducts().subscribe((data)=>{
-    this.trendyProducts=data;
-  })
-}
+  constructor(private productService: ProductService) {}
+
+  ngOnInit(): void {
+    this.productService.popularProducts().subscribe((data) => {
+      this.popularProducts = data;
+    });
+
+    this.productService.trendyProducts().subscribe((data) => {
+      this.trendyProducts = data;
+    });
+  }
+
+  AddToCart(productId: string): void {
+    this.productService.getProduct(productId).subscribe((productData: product) => {
+      if (productData) {
+        productData.quantity = 1; // Default quantity; adjust if needed
+  
+        if (!localStorage.getItem('user')) {
+          // If user is not logged in, add to local cart
+          this.productService.localAddToCart(productData);
+          console.log('Product added to local cart');
+        } else {
+          // If user is logged in, add to server cart
+          const user = localStorage.getItem('user');
+          const userId = user ? JSON.parse(user).id : null;
+  
+          if (userId) {
+            const cartData: cart = {
+              ...productData,
+              productId: productData.id,
+              userId,
+            };
+            delete cartData.id;
+  
+            this.productService.addToCart(cartData).subscribe((result) => {
+              if (result) {
+                console.log('Product added to server cart');
+                this.productService.getCartList(userId); // Update cart list
+              }
+            });
+          }
+        }
+      }
+    });
+  }
+  
 }
